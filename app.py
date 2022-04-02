54d3bc9e8e948d02989b8f0b4dd553f43a20dd35
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2022-04-02 15:28:55 +0600

    update the files

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from streamlit_lottie import st_lottie_spinner
 import requests 
 import itertools
 import streamlit.components.v1 as components
+import streamlit_authenticator as stauth
 
 # Page Config
 st.set_page_config(layout="wide")
@@ -82,7 +83,7 @@ if(menu_id=="Home"):
         ############### Search ##################
 
     query = st.text_input("", "Search...")
-    query_manual = "car"
+    query_manual = "bike"
     print(len(query))
     button_clicked = st.button("Find")   
     ###################### END SEARCH ###############
@@ -127,48 +128,111 @@ if(menu_id=="Home"):
     # indices_on_page, images_on_page = map(list, zip(*image_iterator))
     # st.image(images_on_page, width=100, caption=indices_on_page)
 
-st.markdown("<h5 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----WHY CHOOSE US------</h5>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; font-family:  cursive, sans-serif; color: black;'>Because we care about your safety..</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----WHY CHOOSE US------</h5>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; font-family:  cursive, sans-serif; color: black;'>Because we care about your safety..</h3>", unsafe_allow_html=True)
 
 
-### Safety Signs #########
-col1, col2, col3, col4 = st.columns(4)
+    ### Safety Signs #########
+    col1, col2, col3, col4 = st.columns(4)
 
-with col1:
-    
-    st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_mask.png")
-    st.subheader("Ensuring Mask")
-with col2:
-    st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_24_7.png")
-    st.subheader("24/7 Support")
-with col3:
-    st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_sanitized.png")
-    st.subheader("Sanitising Hands & Equipment")
-
-with col4:
-    st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_gloves.png")
-    st.subheader("Ensuring Gloves")
-#########
-
-###### How It Works ##########
-st.markdown("<h5 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----HOW IT WORKS------</h5>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; font-family:  cursive, sans-serif; color: black;'>Easiest way to get a service</h3>", unsafe_allow_html=True)
-
-#### Step by step ###
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.video("https://www.youtube.com/watch?v=kdRSObObCy4")
-with col2:
-    col1, col2, col3 = st.columns(3)
     with col1:
-        st.text("Select the Service")
-        st.image("")
+        
+        st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_mask.png")
+        st.subheader("Ensuring Mask")
     with col2:
-        st.text("Pick your schedule")
-        st.image("")
+        st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_24_7.png")
+        st.subheader("24/7 Support")
     with col3:
-        st.text("Place Your Order & Relax")
-        st.image("")
-############
+        st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_sanitized.png")
+        st.subheader("Sanitising Hands & Equipment")
+
+    with col4:
+        st.image("https://cdn-marketplacexyz.s3.ap-south-1.amazonaws.com/sheba_xyz/images/png/usp_gloves.png")
+        st.subheader("Ensuring Gloves")
+    #########
+
+    ###### How It Works ##########
+    st.markdown("<h5 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----HOW IT WORKS------</h5>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; font-family:  cursive, sans-serif; color: black;'>Easiest way to get a service</h3>", unsafe_allow_html=True)
+
+    #### Step by step ###
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.video("https://www.youtube.com/watch?v=kdRSObObCy4")
+    with col2:
+        working_array = {"Select the Service","Pick your schedule","Place Your Order & Relax"}
+
+    ############
+        st.markdown(
+                            " ".join([
+                                "<div class='r-text-recipe'>",
+                                # "<div class='food-title'>",
+                                # #f"<img src='{food_image}' />",
+                                # #f"<h2 class='font-title text-bold'>{title}</h2>",
+                                # "</div>",
+                                '<div class="divider"><div class="divider-mask"></div></div>',
+                                #"<h3 class='ingredients font-body text-bold'>Ingredients</h3>",
+                                "<ul class='ingredients-list font-body'>",
+                                " ".join([f'<li><h4 >{item}</h4></li>' for item in working_array]),
+                                "</ul>",
+                                # "<h3 class='directions font-body text-bold'>Directions</h3>",
+                                # "<ol class='ingredients-list font-body'>",
+                                # " ".join([f'<li>{item}</li>' for item in directions]),
+                                # "</ol>",
+                                "</div>"
+                            ]),
+                            unsafe_allow_html=True
+                        )
+
+    st.markdown("<h5 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----CONTACT US------</h5>", unsafe_allow_html=True)
+    st.text("16516 |info@smartgarage.com Corporate Address | House # 57, Road # 25, Block - A,Banani, Dhaka 1212")
+
+elif(menu_id=="Login"):
+    names = ['Sadia','Tonmoy']
+    usernames = ['sadia','tonmoy']
+    passwords = ['123','456']
+    #hashed_passwords = stauth.Hasher(passwords).generate()
+  #  authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+    #'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+   # name, authentication_status, username = authenticator.login('Login','main')
+    flag = True
+    if(flag):
+        st.markdown("<h2 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----LOGIN------</h2>", unsafe_allow_html=True)
+        my_form = st.form(key='form-1')
+
+        username =my_form.text_input("UserName")
+        passwords = my_form.text_input("Password")
+        submit=my_form.form_submit_button('Submit')
+    if(submit=='Submit'):
+            flag= False
+            st.balloons()
+            st.write("Welcome")
+
+elif (menu_id=="Registration"):
+    st.markdown("<h2 style='text-align: center; font-family:  cursive, sans-serif; color: #B3E1E2;'>-----Registration------</h2>", unsafe_allow_html=True)
+    # creating a form
+    my_form=st.form(key='form-1')
+    # creating input fields
+    fname=my_form.text_input('First Name:')
+    lname=my_form.text_input('Last Name:')
+    email=my_form.text_input('Email:')
+    # creating radio button 
+    gender=my_form.radio('Gender',('Male','Female'))
+    # creating slider 
+    age=my_form.slider('Age:',1,120)
+    # creating date picker
+    bday=my_form.date_input('Enter Birthdate:')
+    # creating a text area
+    address=my_form.text_area('Enter Address:')
+    # creating a submit button
+    submit=my_form.form_submit_button('Submit')
+    # the following gets updated after clicking on submit, printing the details of the fields that are submitted in the form
+    if(submit=='Submit'):
+        st.write('Name is '+fname+' '+lname)
+        st.write('Email is '+email)
+        st.write('Gender is '+gender)
+        st.write('Age is '+str(age))
+        st.write('Birthday is '+str(bday))
+        st.write('Address is '+address)
